1bd560f1e059fc2322d96d52d5b0c156c2d9c7cf
HPCesia <me@hpcesia.com>
2026-07-10 22:50:01 +0800

    update package: helixPlugins.forest: unstable-2026-07-08 -> unstable-2026-07-10

--- pkgs/helix-plugins/forest.nix
+++ pkgs/helix-plugins/forest.nix
@@ -6,13 +6,13 @@
 }:
 buildHelixPlugin {
   pname = "forest.hx";
-  version = "0-unstable-2026-07-08";
+  version = "0-unstable-2026-07-10";
 
   src = fetchFromGitHub {
     owner = "Ra77a3l3-jar";
     repo = "forest.hx";
-    rev = "3c599e23c05af95b799f8560c2489cc2821d9c91";
-    hash = "sha256-scg0cV4yQoyf/9MzMNvWnDi5cbjTqLO7KGANc6DqAt0=";
+    rev = "bc7fea7bbc70c285357e515af30678c26a100e7d";
+    hash = "sha256-HP/ISHRNs6Se5im54QeT17mO5cs2jD3D5TvBrwJP55k=";
   };
 
   meta = {
