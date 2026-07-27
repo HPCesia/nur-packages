61ac36a1da0aac4a92442db14c27fab300e25c32
HPCesia <me@hpcesia.com>
2026-07-27 12:12:09 +0800

    fix: steelix's treesitter grammar

--- pkgs/steelix/default.nix
+++ pkgs/steelix/default.nix
@@ -3,10 +3,54 @@
   lib,
   callPackage,
   helix,
+  tree-sitter-grammars,
+  lockedGrammars ? lib.filterAttrs (name: _: tree-sitter-grammars ? "tree-sitter-${name}") (lib.importJSON ./grammars.json),
   steelix-unwrapped ? callPackage ./unwrapped.nix {},
 }:
 (helix.override {
   helix-unwrapped = steelix-unwrapped;
+  lockedGrammars = lockedGrammars;
+  grammarsOverlay = final: prev: {
+    tree-sitter-agda = prev.tree-sitter-agda.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-beancount = prev.tree-sitter-beancount.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-git-rebase = prev.tree-sitter-git-rebase.overrideAttrs {
+      dontPatch = true;
+    };
+    tree-sitter-glimmer = prev.tree-sitter-glimmer.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-janet-simple = prev.tree-sitter-janet-simple.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-qmljs = prev.tree-sitter-qmljs.overrideAttrs {
+      dontCheckForBrokenSymlinks = true;
+    };
+    tree-sitter-sql = prev.tree-sitter-sql.override {
+      generate = false;
+    };
+    tree-sitter-strace = prev.tree-sitter-strace.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-tact = prev.tree-sitter-tact.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-tlaplus = prev.tree-sitter-tlaplus.overrideAttrs {
+      dontPatch = true;
+    };
+    tree-sitter-vue = prev.tree-sitter-vue.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-wit = prev.tree-sitter-wit.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+    tree-sitter-yuck = prev.tree-sitter-yuck.override {
+      excludeBrokenTreeSitterJson = false;
+    };
+  };
 }).overrideAttrs
 (
   _: previousAttrs: {
