64ea41a0e254a7f35748b4b796dcc34119f429ec
Ineffa <ineffa@noreply.git.trin.one>
2026-09-15 20:55:29 +0000

    steelix: 0-unstable-2026-08-31 -> 0-unstable-2026-09-14

--- pkgs/steelix/unwrapped.nix
+++ pkgs/steelix/unwrapped.nix
@@ -8,13 +8,13 @@ helix-unwrapped.overrideAttrs (
   finalAttrs: _: {
     pname = "steelix-unwrapped";
 
-    version = "0-unstable-2026-08-31";
+    version = "0-unstable-2026-09-14";
 
     src = fetchFromGitHub {
       owner = "mattwparas";
       repo = "helix";
-      rev = "ba5b022c1000a0ce28d4ce1d09acdd062a83a020";
-      hash = "sha256-vJ7VgxuM/Dp7vyVlu6EXjP/ES14TALy64jgzyuYZl6g=";
+      rev = "02c1bd1ac9a4a2376207990179e6b316f49f86ed";
+      hash = "sha256-+mEYu9Mk4zHW9rz2B21/Z3dvtg8oEbLtcEpeG5Yyf9s=";
     };
 
     cargoDeps = rustPlatform.fetchCargoVendor {
